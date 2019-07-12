a433d56d57e9c1528b07b0bc1a5dc63e72c2b803
Kyle <kyleagren@gmail.com>
2019-07-11 21:57:13 -0600

    Added Parallax BG, added dashes, adjusted tilemap to be able to be walked on.
    Cleaned up dash code, fiddled with platforms so that the character could walk over them, added dash timer / cooldown etc. Dash animation looks great.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,22 +2,30 @@ extends KinematicBody2D
 
 var motion = Vector2()
 var speed = 1000
-var number_of_jumps = 5
-var jump_counter
+var number_of_jumps = 1
+var jump_counter = 0
+var dash_counter = 0
 var can_double_jump
-
+var is_dashing = false
+var dash_available = true
+var number_of_dashes = 0
+var can_wall_run = false
 
 const GRAVITY = 100
 const UP = Vector2(0, -1)
 const JUMP_SPEED = 2000
-const HEIGHT_BOUNDARY = 10000
+const HEIGHT_BOUNDARY = 8000
 const SLOWNESS_MODIFIER = 0.7
+const DASH_DISTANCE = 3000
+const DASH_ANGLE = deg2rad(90)
 
 
 
 func _physics_process(delta):
+	dash()
 	apply_gravity()
-	wall_run()
+	if can_wall_run:
+		wall_run()
 	move()
 	jump()
 	animate()
@@ -25,13 +33,13 @@ func _physics_process(delta):
 	lose()
 
 func move():
-	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right") and not is_dashing:
 		motion.x = -speed
 		$Sprite.flip_h = true
-	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left") and not is_dashing:
 		motion.x = speed
 		$Sprite.flip_h = false
-	else:
+	elif not is_dashing:
 		motion.x = 0
 
 
@@ -62,12 +70,32 @@ func wall_run():
 				motion.x = -JUMP_SPEED
 
 
+func dash():
+	if Input.is_action_just_pressed("dash") and not $Sprite.is_flipped_h() and dash_available and not dash_counter == number_of_dashes:
+		is_dashing = true
+		dash_counter += 1
+		$Sprite.rotation = DASH_ANGLE
+		$DashTimer.start()
+		motion.y = 0
+		motion.x += DASH_DISTANCE
+	elif Input.is_action_just_pressed("dash") and $Sprite.is_flipped_h() and dash_available and not dash_counter == number_of_dashes:
+		is_dashing = true
+		dash_counter += 1
+		$Sprite.rotation = -DASH_ANGLE
+		$DashTimer.start()
+		motion.y = 0
+		motion.x -= DASH_DISTANCE
+		
+	if dash_counter == number_of_dashes and is_on_floor():
+		dash_counter = 0
+
+
 func apply_gravity():
-	if is_on_floor() and motion.y > 0:
+	if is_on_floor() and motion.y > 0 and not is_dashing:
 		motion.y = GRAVITY
 	elif is_on_ceiling():
 		motion.y = 1
-	else:
+	elif not is_dashing:
 		motion.y += GRAVITY
 
 
@@ -90,3 +118,15 @@ func lose():
 
 func _on_JumpTimer_timeout():
 	can_double_jump = true
+
+
+func _on_DashTimer_timeout():
+	is_dashing = false
+	$Sprite.rotation = 0
+	dash_available = false
+	$DashRecharge.start()
+
+
+func _on_DashRecharge_timeout():
+	dash_available = true
+	
